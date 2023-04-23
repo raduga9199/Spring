ed7ac7517934ca47d06e56e24089a3d9aed70290
raduga9199 <raduga9199@gmail.com>
2023-04-23 15:46:25 -0400

    Dealer App  (Wiring and Auto wiring)

--- Spring-02-Core-Wiring/src/main/java/com/cydeo/Person.java
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/Person.java
@@ -2,9 +2,11 @@ package com.cydeo;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class Person {
     private String name;
     private Car car;
