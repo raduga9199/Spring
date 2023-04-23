fa36e253108db77c2cc368924f5503d3a2bd4b92
raduga9199 <raduga9199@gmail.com>
2023-04-23 18:26:36 -0400

    Spring Abstraction

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/JavaClass.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+public class JavaClass {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 30000;
+        int i = 900000000;
+        long l = 900000000;
+        double d = 1000000000.99;
+        float f = 1000000000000000000000000000000000000f;
+        char c = 'a';
+        boolean bool = true;
+        String str = "abcdefg";
+    }
+}
