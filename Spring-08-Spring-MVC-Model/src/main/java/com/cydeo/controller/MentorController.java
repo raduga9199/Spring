b37d75cda5e0aba4902f86b8f36c690ef5dd6906
raduga9199 <raduga9199@gmail.com>
2023-04-24 16:48:28 -0400

    Nice Look Table Creation

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -2,23 +2,28 @@ package com.cydeo.controller;
 
 import com.cydeo.model.Mentor;
 import com.cydeo.enums.Gender;
+import com.sun.jdi.request.StepRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@RequestMapping("mentor")
 public class MentorController {
 
-    @RequestMapping("/mentor-list")
+    @RequestMapping("/list")
     public String showTable(Model model){
-        List<Mentor> mentorList = new ArrayList<>();
-
-        mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
-        mentorList.add(new Mentor("Tom","Hanks",65,Gender.MALE));
-        mentorList.add(new Mentor("Ammy","Bryan",25,Gender.FEMALE));
+        List<Mentor> mentorList = new ArrayList<>(
+                Arrays.asList(
+                        new Mentor("Mike","Smith",45,Gender.MALE),
+                        new Mentor("Tom","Hanks",65,Gender.MALE),
+                        new Mentor("Ammy","Bryan",25,Gender.FEMALE)
+                )
+        );
 
         model.addAttribute("mentors",mentorList);
 
