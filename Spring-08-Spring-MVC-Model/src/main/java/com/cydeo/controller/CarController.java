a260d82fbef1fc4e0666f7af546af7503c8cb961
raduga9199 <raduga9199@gmail.com>
2023-04-24 17:17:41 -0400

    @PathVariables

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,34 @@
+package com.cydeo.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make,Model model){
+        model.addAttribute("make",make);
+        System.out.println(make);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make",required = false,defaultValue = "Tesla")String make, Model model){
+        model.addAttribute("make",make);
+        return "car/car-info";
+    }
+
+
+    @RequestMapping("/info/{make}/{year}")
+    public String getCarInfo(@PathVariable String make,@PathVariable int year, Model model){
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        return "car/car-info";
+    }
+}
