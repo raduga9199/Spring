e939635852c24e06c28f9ee5c0a9c86777f1eea6
raduga9199 <raduga9199@gmail.com>
2023-04-23 22:36:44 -0400

    MVC

--- Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
     public String home2(){
         return "welcome.html";
     }
+
+    @RequestMapping("/")
+    public String home3(){
+        return "welcome.html";
+    }
 }
