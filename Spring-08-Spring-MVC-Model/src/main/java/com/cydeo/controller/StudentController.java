ffa6c57f4aaa57773d2b8e98ce5ead7a5aac7512
raduga9199 <raduga9199@gmail.com>
2023-04-24 11:15:42 -0400

    Spring MVC Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
@@ -11,7 +15,22 @@ public class StudentController {
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
 
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
 
-        return "/student/welcome";
+        int studentID = new Random().nextInt();
+        model.addAttribute("id",studentID);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(8);
+        numbers.add(10);
+
+        model.addAttribute("numbers",numbers);
+
+
+        return "student/welcome";
     }
 }
