699379eecfd71b0d5e9c85a0394339364493fb2f
raduga9199 <raduga9199@gmail.com>
2023-04-25 13:40:18 -0400

    Spring Thymeleaf
    - GET, POST forms

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,22 +3,24 @@ package com.cydeo.controller;
 import com.cydeo.boostrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
-
         return "student/welcome";
     }
 }
